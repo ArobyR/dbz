6c03093718e75a245e11fdf4c7adb8dbca58a606
ArobyR <ar.rosario001@gmail.com>
2021-07-27 22:27:40 -0400

    update AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Header from "../components/Header";
+import CharaterScreen from "../pages/CharaterScreen";
 import MenScreen from "../pages/MenScreen";
 import SearchScreen from "../pages/SearchScreen";
 import WomenScreen from "../pages/WomenScreen";
@@ -13,6 +14,7 @@ const AppRouter = () => {
         <Route exact path="/men" component={MenScreen} />
         <Route exact path="/women" component={WomenScreen} />
         <Route exact path="/search" component={SearchScreen} />
+        <Route exact path="/character/:id" component={CharaterScreen} />
         {/* <Redirect to="/men" /> */}
       </Switch>
     </>
