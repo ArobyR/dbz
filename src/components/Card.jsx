219cd61546cc61cd4f1667db85321f50c676c2e2
ArobyR <ar.rosario001@gmail.com>
2021-07-27 22:25:59 -0400

    update components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Card = ({id, name, description, type}) => {
-    
-    const path = `/assets/${type}-${id}.png`
-    
-    return (
-        <div className="card m-3 col-12 col-md-4 p-2" style={{ width: 250}}>
-            <img className="card-img-top" src={path} alt={id}/>
+const Card = ({ id, name, type }) => {
+  const path = `/assets/${type}-${id}.png`;
 
-            <div className="car-body">
-              <h3 className="card-title">{name}</h3>
-                <p className="card-text">
-                    <Link className="card-link" to="">
-                        Ver mas...
-                    </Link>
-                </p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="card m-3 col-12 col-md-4 p-2" style={{ width: 250 }}>
+      <img className="card-img-top" src={path} alt={id} />
 
-export default Card
+      <div className="car-body">
+        <h3 className="card-title">{name}</h3>
+        <p className="card-text">
+          <Link className="card-link" to={`/character/${id}`}>
+            Ver mas...
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
