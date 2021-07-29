2cde508784c6ca002e2d956d2c8072954a48570e
ArobyR <ar.rosario001@gmail.com>
2021-07-28 23:47:17 -0400

    add context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useEffect, useReducer } from "react";
+import LoginRouter from "./routes/LoginRouter";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { useState } from "react";
-import LoginRouter from "./routes/LoginRouter";
+import { AuthContext } from "./context/AuthContext";
+import { AuthReducer } from "./reducers/AuthReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("log")) || { log: false };
+};
 
 function App() {
+  const [log, dispatch] = useReducer(AuthReducer, {}, init);
+
+  useEffect(() => {
+    localStorage.setItem("log", JSON.stringify(log));
+  }, [log]);
 
   return (
-    <div>
+    <AuthContext.Provider value={{ log, dispatch }}>
       <LoginRouter />
-    </div>
+    </AuthContext.Provider>
   );
 }
 
